a32e9ee385a668d55e8bbc57cba2e5e87a77013a
bunjee <bunjee@omega.gg>
2022-01-18 22:47:50 +0100

    Porting to qt6 (36)

--- deploy.sh
+++ deploy.sh
@@ -474,8 +474,8 @@ elif [ $1 = "macOS" ]; then
     install_name_tool -change @rpath/QtDBus.framework/Versions/$qx/QtDBus \
                               @loader_path/../QtDBus.dylib platforms/libqcocoa.dylib
 
-    install_name_tool -change @rpath/QtPrintSupport.framework/Versions/$qx/QtPrintSupport \
-                              @loader_path/../QtPrintSupport.dylib platforms/libqcocoa.dylib
+    #install_name_tool -change @rpath/QtPrintSupport.framework/Versions/$qx/QtPrintSupport \
+    #                          @loader_path/../QtPrintSupport.dylib platforms/libqcocoa.dylib
 
     otool -L platforms/libqcocoa.dylib
 
@@ -501,8 +501,8 @@ elif [ $1 = "macOS" ]; then
     #install_name_tool -change @rpath/QtWidgets.framework/Versions/$qx/QtWidgets \
     #                          @loader_path/../QtWidgets.dylib imageformats/libqsvg.dylib
 
-    install_name_tool -change @rpath/QtSvg.framework/Versions/$qx/QtSvg \
-                              @loader_path/../QtSvg.dylib imageformats/libqsvg.dylib
+    #install_name_tool -change @rpath/QtSvg.framework/Versions/$qx/QtSvg \
+    #                          @loader_path/../QtSvg.dylib imageformats/libqsvg.dylib
 
     otool -L imageformats/libqsvg.dylib
 
@@ -518,42 +518,42 @@ elif [ $1 = "macOS" ]; then
     #install_name_tool -change @rpath/QtQuick.framework/Versions/$qx/QtQuick \
     #                          @loader_path/../QtQuick.dylib $QtQuick/libqtquick2plugin.dylib
 
-    #if [ -f QtQmlModels.dylib ]; then
+    if [ -f QtQmlModels.dylib ]; then
 
         #install_name_tool -change @rpath/QtQmlModels.framework/Versions/$qx/QtQmlModels \
         #                          @loader_path/../QtQmlModels.dylib \
         #                          $QtQuick/libqtquick2plugin.dylib
 
-        #install_name_tool -change @rpath/QtQmlWorkerScript.framework/Versions/$qx/QtQmlWorkerScript \
-        #                          @loader_path/../QtQmlWorkerScript.dylib \
-        #                          $QtQuick/libqtquick2plugin.dylib
-    #fi
+        install_name_tool -change @rpath/QtQmlWorkerScript.framework/Versions/$qx/QtQmlWorkerScript \
+                                  @loader_path/../QtQmlWorkerScript.dylib \
+                                  $QtQuick/libqtquick2plugin.dylib
+    fi
 
     otool -L $QtQuick/libqtquick2plugin.dylib
 
     #----------------------------------------------------------------------------------------------
     # WorkerScript
 
-    if [ $qt = "qt6" ]; then
+    #if [ $qt = "qt6" ]; then
 
-        install_name_tool -change @rpath/QtCore.framework/Versions/$qx/QtCore \
-                                  @loader_path/../../QtCore.dylib \
-                                  QtQml/WorkerScript/libworkerscriptplugin.dylib
+        #install_name_tool -change @rpath/QtCore.framework/Versions/$qx/QtCore \
+        #                          @loader_path/../../QtCore.dylib \
+        #                          QtQml/WorkerScript/libworkerscriptplugin.dylib
 
-        install_name_tool -change @rpath/QtNetwork.framework/Versions/$qx/QtNetwork \
-                                  @loader_path/../../QtNetwork.dylib \
-                                  QtQml/WorkerScript/libworkerscriptplugin.dylib
+        #install_name_tool -change @rpath/QtNetwork.framework/Versions/$qx/QtNetwork \
+        #                          @loader_path/../../QtNetwork.dylib \
+        #                          QtQml/WorkerScript/libworkerscriptplugin.dylib
 
-        install_name_tool -change @rpath/QtQml.framework/Versions/$qx/QtQml \
-                                  @loader_path/../../QtQml.dylib \
-                                  QtQml/WorkerScript/libworkerscriptplugin.dylib
+        #install_name_tool -change @rpath/QtQml.framework/Versions/$qx/QtQml \
+        #                          @loader_path/../../QtQml.dylib \
+        #                          QtQml/WorkerScript/libworkerscriptplugin.dylib
 
-        install_name_tool -change @rpath/QtQmlWorkerScript.framework/Versions/$qx/QtQmlWorkerScript \
-                                  @loader_path/../../QtQmlWorkerScript.dylib \
-                                  QtQml/WorkerScript/libworkerscriptplugin.dylib
+        #install_name_tool -change @rpath/QtQmlWorkerScript.framework/Versions/$qx/QtQmlWorkerScript \
+        #                          @loader_path/../../QtQmlWorkerScript.dylib \
+        #                          QtQml/WorkerScript/libworkerscriptplugin.dylib
 
-        otool -L QtQml/WorkerScript/libworkerscriptplugin.dylib
-    fi
+        #otool -L QtQml/WorkerScript/libworkerscriptplugin.dylib
+    #fi
 
     #----------------------------------------------------------------------------------------------
     # VLC
