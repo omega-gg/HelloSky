334e2bfddb446df7c77f9d1e3b435d356e94bdc8
bunjee <bunjee@omega.gg>
2022-01-18 11:36:08 +0100

    Porting to qt6 (28)

--- build.sh
+++ build.sh
@@ -206,9 +206,9 @@ if [ $1 = "win32" -o $1 = "win64" ]; then
 
         if [ $1 = "win32" ]; then
 
-            target="x86"
+            abi="x86"
         else
-            target="x64"
+            abi="x64"
         fi
     fi
 
@@ -297,8 +297,8 @@ elif [ $compiler = "msvc" ]; then
         spec=win32-msvc
     fi
 
-    PATH="$jom:$MSVC/bin/Host$target/$target:\
-$WindowsKit/bin/$WindowsKit_version/$target:\
+    PATH="$jom:$MSVC/bin/Host$abi/$abi:\
+$WindowsKit/bin/$WindowsKit_version/$abi:\
 $Qt/bin:$PATH"
 
     export INCLUDE="$MSVC/include:\
@@ -306,9 +306,9 @@ $WindowsKit/Include/$WindowsKit_version/ucrt:\
 $WindowsKit/Include/$WindowsKit_version/um:\
 $WindowsKit/Include/$WindowsKit_version/shared"
 
-    export LIB="$MSVC/lib/$target:\
-$WindowsKit/Lib/$WindowsKit_version/ucrt/$target:\
-$WindowsKit/Lib/$WindowsKit_version/um/$target"
+    export LIB="$MSVC/lib/$abi:\
+$WindowsKit/Lib/$WindowsKit_version/ucrt/$abi:\
+$WindowsKit/Lib/$WindowsKit_version/um/$abi"
 
 elif [ $1 = "macOS" ]; then
 
