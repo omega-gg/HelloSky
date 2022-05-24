fc2f24db8fa9a4fc1cf79830b2ea8caba9f6570c
bunjee <bunjee@omega.gg>
2022-05-24 18:31:24 +0200

    Update to JDK 11.0.2

--- build.sh
+++ build.sh
@@ -39,7 +39,7 @@ WindowsKit_version="10"
 #--------------------------------------------------------------------------------------------------
 # Android
 
-JDK_version="8u321"
+JDK_version="11.0.2"
 
 SDK_version="30"
 SDK_version_minimum="21"
