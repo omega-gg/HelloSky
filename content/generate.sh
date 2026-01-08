15bafebb2f0f11d1d862e3e84f831d26e7253f0c
bunjee <bunjee@omega.gg>
2026-01-08 15:32:50 +0100

    Update generate.sh

--- content/generate.sh
+++ content/generate.sh
@@ -295,8 +295,12 @@ else
     if [ $1 = "linux" ]; then
 
         imports="QtQuick=2.7"
-    else
+
+    elif [ $qt = "qt5" ]; then
+
         imports="QtQuick=2.14"
+    else
+        imports="QtQuick"
     fi
 fi
 
